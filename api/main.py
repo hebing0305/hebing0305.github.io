cd201170101d2071a67038c1bcc1558dc8e2ce12
贺兵 <764010928@qq.com>
2017-12-27 17:35:49 +0800

    修复json出错的问题

--- api/main.py
+++ api/main.py
@@ -1,104 +1,98 @@
 {
-    "newsList": [
-        {
-            "content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
-            "hint": "查看兼容方案…",
-            "id": 0,
-            "title": "Android8.0运行时权限策略变化",
-            "url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
-        },
-        {
-            "content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
-            "hint": "去瞅瞅…",
-            "id": 1,
-            "title": "轻量级相册",
-            "url": "https://github.com/yanzhenjie/Ablum"
-        },
+	"newsList": [{
+		"content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
+		"hint": "查看兼容方案…",
+		"id": 0,
+		"title": "Android8.0运行时权限策略变化",
+		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
+	},
+	{
+		"content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
+		"hint": "去瞅瞅…",
+		"id": 1,
+		"title": "轻量级相册",
+		"url": "https://github.com/yanzhenjie/Ablum"
+	},
+	{
+		"content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
+		"hint": "查看兼容方案…",
+		"id": 0,
+		"title": "Android8.0运行时权限策略变化",
+		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
+	},
+	{
+		"content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
+		"hint": "去瞅瞅…",
+		"id": 1,
+		"title": "轻量级相册",
+		"url": "https://github.com/yanzhenjie/Ablum"
+	},
+	{
+		"content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
+		"hint": "查看兼容方案…",
+		"id": 0,
+		"title": "Android8.0运行时权限策略变化",
+		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
+	},
+	{
+		"content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
+		"hint": "去瞅瞅…",
+		"id": 1,
+		"title": "轻量级相册",
+		"url": "https://github.com/yanzhenjie/Ablum"
+	},
+	{
+		"content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
+		"hint": "查看兼容方案…",
+		"id": 0,
+		"title": "Android8.0运行时权限策略变化",
+		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
+	},
+	{
+		"content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
+		"hint": "去瞅瞅…",
+		"id": 1,
+		"title": "轻量级相册",
+		"url": "https://github.com/yanzhenjie/Ablum"
+	}],
+	"person": {
+		"avatars": "./images/main/yzj_head.png",
+		"name": "贺兵",
+		"slogan": "就算不想前进，命运也会推着你走吧～",
+		"tabTags": [{
+			"hint": "我的Github",
+			"link": "https://github.com/hebing0305"
+		},
 		{
-            "content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
-            "hint": "查看兼容方案…",
-            "id": 0,
-            "title": "Android8.0运行时权限策略变化",
-            "url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
-        },
-        {
-            "content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
-            "hint": "去瞅瞅…",
-            "id": 1,
-            "title": "轻量级相册",
-            "url": "https://github.com/yanzhenjie/Ablum"
-        }
+			"hint": "我的微博",
+			"link": "https://weibo.com/hebinga"
+		},
 		{
-            "content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
-            "hint": "查看兼容方案…",
-            "id": 0,
-            "title": "Android8.0运行时权限策略变化",
-            "url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
-        },
-        {
-            "content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
-            "hint": "去瞅瞅…",
-            "id": 1,
-            "title": "轻量级相册",
-            "url": "https://github.com/yanzhenjie/Ablum"
-        }
-		{
-            "content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
-            "hint": "查看兼容方案…",
-            "id": 0,
-            "title": "Android8.0运行时权限策略变化",
-            "url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
-        },
-        {
-            "content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
-            "hint": "去瞅瞅…",
-            "id": 1,
-            "title": "轻量级相册",
-            "url": "https://github.com/yanzhenjie/Ablum"
-        }
-    ],
-    "person": {
-        "avatars": "./images/main/yzj_head.png",
-        "name": "贺兵",
-        "slogan": "就算不想前进，命运也会推着你走吧～",
-        "tabTags": [
-            {
-                "hint": "我的Github",
-                "link": "https://github.com/hebing0305"
-            },
-            {
-                "hint": "我的微博",
-                "link": "https://weibo.com/hebinga"
-            },
-            {
-                "hint": "我的知乎",
-                "link": "https://www.zhihu.com/people/hebing0305"
-            }
-        ]
-    },
-    "projectList": [
-        {
-            "describe": "Android状态栏一体化开源库，支持状态栏颜色随着滑动渐变，颜色随着滑动透明，支持使用Drawable和Color作为状态栏背景。",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/StatusView",
-            "name": "StatusView"
-        },
-        {
-            "describe": "Android平台的MD风格的相册，支持多选、相机、画廊、图片缩放查看等，推荐和图片裁剪工具Durban结合使用。",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/Album",
-            "name": "Album"
-        },
-        {
-            "describe": "Android平台的MD风格的图片裁剪工具，可以一次裁剪多长图片，推荐和相册Album结合使用。",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/Durban",
-            "name": "Durban"
-        }
-    ],
-    "wechat": {
-        "hint": "/n扫一扫关注贺兵",
-        "image": "./images/main/yzj_wechat.jpg"
-    },
-    "field": "value"
+			"hint": "我的知乎",
+			"link": "https://www.zhihu.com/people/hebing0305"
+		}]
+	},
+	"projectList": [{
+		"describe": "Android状态栏一体化开源库，支持状态栏颜色随着滑动渐变，颜色随着滑动透明，支持使用Drawable和Color作为状态栏背景。",
+		"id": 0,
+		"link": "https://github.com/yanzhenjie/StatusView",
+		"name": "StatusView"
+	},
+	{
+		"describe": "Android平台的MD风格的相册，支持多选、相机、画廊、图片缩放查看等，推荐和图片裁剪工具Durban结合使用。",
+		"id": 0,
+		"link": "https://github.com/yanzhenjie/Album",
+		"name": "Album"
+	},
+	{
+		"describe": "Android平台的MD风格的图片裁剪工具，可以一次裁剪多长图片，推荐和相册Album结合使用。",
+		"id": 0,
+		"link": "https://github.com/yanzhenjie/Durban",
+		"name": "Durban"
+	}],
+	"wechat": {
+		"hint": "/n扫一扫关注贺兵",
+		"image": "./images/main/yzj_wechat.jpg"
+	},
+	"field": "value"
 }
