be0f707ded974e39754cd0e731190fd9d9536869
贺兵 <764010928@qq.com>
2017-12-27 17:06:45 +0800

    修改了列表布局

--- api/main.py
+++ api/main.py
@@ -14,11 +14,53 @@
             "title": "轻量级相册",
             "url": "https://github.com/yanzhenjie/Ablum"
         }
+		{
+            "content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
+            "hint": "查看兼容方案…",
+            "id": 0,
+            "title": "Android8.0运行时权限策略变化",
+            "url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
+        },
+        {
+            "content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
+            "hint": "去瞅瞅…",
+            "id": 1,
+            "title": "轻量级相册",
+            "url": "https://github.com/yanzhenjie/Ablum"
+        }
+		{
+            "content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
+            "hint": "查看兼容方案…",
+            "id": 0,
+            "title": "Android8.0运行时权限策略变化",
+            "url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
+        },
+        {
+            "content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
+            "hint": "去瞅瞅…",
+            "id": 1,
+            "title": "轻量级相册",
+            "url": "https://github.com/yanzhenjie/Ablum"
+        }
+		{
+            "content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
+            "hint": "查看兼容方案…",
+            "id": 0,
+            "title": "Android8.0运行时权限策略变化",
+            "url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
+        },
+        {
+            "content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
+            "hint": "去瞅瞅…",
+            "id": 1,
+            "title": "轻量级相册",
+            "url": "https://github.com/yanzhenjie/Ablum"
+        }
     ],
     "person": {
         "avatars": "./images/main/yzj_head.png",
         "name": "贺兵",
-        "slogan": "他说正直刚烈，嫉恶如仇，且有勇有谋，文武双全。",
+        "slogan": "就算不想前进，命运也会推着你走吧～",
         "tabTags": [
             {
                 "hint": "我的Github",
@@ -52,52 +94,10 @@
             "id": 0,
             "link": "https://github.com/yanzhenjie/Durban",
             "name": "Durban"
-        },
-        {
-            "describe": "Android局域网服务器，支持部署网站，动态http接口，文件上传下载等，可以作为局域网服务器使用。",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/AndServer",
-            "name": "AndServer"
-        },
-        {
-            "describe": "一个实现Http标准协议的Android网络请求框架，支持缓存，支持自动重定向，支持同步、异步，支持文件下载...",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/NoHttp",
-            "name": "NoHttp"
-        },
-        {
-            "describe": "Android运行时权限管理，链式调用，支持注解回调，支持Listener回调，支持Rationale，支持授权失败后Dialog提示用户在Setting中重新授权...",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/AndPermission",
-            "name": "AndPermission"
-        },
-        {
-            "describe": "RecyclerView Item侧滑菜单，Item侧滑删除，Item长按拖拽...",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/SwipeRecyclerView",
-            "name": "SwipeRecyclerView"
-        },
-        {
-            "describe": "一个Activity多个Fragment的UI架构，支持保存fragment回退栈与状态，支持Fragment之间跳转后onFragmentResult()...",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/NoFragment",
-            "name": "NoFragment"
-        },
-        {
-            "describe": "Android平台MD风格的地址选择器，我倡议大家按照Android的风格来做设计，这个Demo作为一个设计指导吧，有gif图可以供大家参考。",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/AddressChecker",
-            "name": "AddressChecker"
-        },
-        {
-            "describe": "专门为Android平台编译的ZbarSDK，提供远程依赖方式、jar + so的方式、module方式，修复了中文乱码等bug，并附上了编译教程，开发者也可以自己编译。",
-            "id": 0,
-            "link": "https://github.com/yanzhenjie/android-zbar-sdk",
-            "name": "ZbarSDK"
         }
     ],
     "wechat": {
-        "hint": "扫一扫关注贺兵",
+        "hint": "/n 扫一扫关注贺兵",
         "image": "./images/main/yzj_wechat.jpg"
     },
     "field": "value"
