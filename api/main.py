ebf364327a123ac76e8a6c0f32bb93f3d20c7cb3
贺兵 <764010928@qq.com>
2017-12-27 17:40:15 +0800

    ++++++++++++++++++++++++

--- api/main.py
+++ api/main.py
@@ -7,53 +7,39 @@
 		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
 	},
 	{
-		"content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
-		"hint": "去瞅瞅…",
-		"id": 1,
-		"title": "轻量级相册",
-		"url": "https://github.com/yanzhenjie/Ablum"
-	},
-	{
 		"content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
 		"hint": "查看兼容方案…",
-		"id": 0,
+		"id": 1,
 		"title": "Android8.0运行时权限策略变化",
 		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
 	},
 	{
-		"content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
-		"hint": "去瞅瞅…",
-		"id": 1,
-		"title": "轻量级相册",
-		"url": "https://github.com/yanzhenjie/Ablum"
-	},
-	{
 		"content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
 		"hint": "查看兼容方案…",
-		"id": 0,
+		"id": 2,
 		"title": "Android8.0运行时权限策略变化",
 		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
 	},
 	{
-		"content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
-		"hint": "去瞅瞅…",
-		"id": 1,
-		"title": "轻量级相册",
-		"url": "https://github.com/yanzhenjie/Ablum"
-	},
-	{
 		"content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
 		"hint": "查看兼容方案…",
-		"id": 0,
+		"id": 3,
 		"title": "Android8.0运行时权限策略变化",
 		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
 	},
 	{
-		"content": "一个支持选照片、选视频、拍照片、拍视频、画廊浏览网络图和本地图的轻量级相册库！",
-		"hint": "去瞅瞅…",
-		"id": 1,
-		"title": "轻量级相册",
-		"url": "https://github.com/yanzhenjie/Ablum"
+		"content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
+		"hint": "查看兼容方案…",
+		"id": 4,
+		"title": "Android8.0运行时权限策略变化",
+		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
+	},
+	{
+		"content": "在 Android O 之前，如果应用在运行时请求权限并且被授予该权限，系统会错误地将属于同一权限组并且在清单中注册的其他权限也一起授予应用。对于针对Android O的应用，此行为已被纠正。系统只会授予应用明确请求的权限。然而一旦用户为应用授予某个权限，则所有后续对该权限组中权限的请求都将被自动批准。",
+		"hint": "查看兼容方案…",
+		"id": 5,
+		"title": "Android8.0运行时权限策略变化",
+		"url": "http://blog.csdn.net/yanzhenjie1003/article/details/76719487"
 	}],
 	"person": {
 		"avatars": "./images/main/yzj_head.png",
